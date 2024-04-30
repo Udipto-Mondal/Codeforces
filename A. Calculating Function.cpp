d121c2e498777c34d556c0ffe898fae8656d1cbc
Udipto <131182197+Udipto-Mondal@users.noreply.github.com>
2024-04-30 21:41:26 +0600

    Add files via upload

---
+++ A. Calculating Function.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author:  Udipto
+ *    created:Tuesday
+30 Apr 2024
+Dhaka, Bangladesh
+**/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    if (n%2==0)
+    {
+        cout<<n/2<<"\n";
+    }
+    else
+       cout<<(n/2)-n<<"\n";
+
+return 0;
+}
