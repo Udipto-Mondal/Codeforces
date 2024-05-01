a0cde3d80f5ba292b425c460ff22701ac8ff09f3
Udipto <131182197+Udipto-Mondal@users.noreply.github.com>
2024-05-01 23:30:43 +0600

    Add files via upload

---
+++ A. Wrong Subtraction.cpp
@@ -0,0 +1,17 @@
+/**
+ *    author:  Udipto
+ *    created:
+**/
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+
+	for (int i = 0; i < k; ++i) {
+		if (n % 10 == 0) n /= 10;
+		else n--;}
+    cout<<n;
+    return 0;
+}
